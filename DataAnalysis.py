c6a52e99904cc15c35329d6fcf98a5886b23dbd6
hansgutts <hansgutts@gmail.com>
2023-10-31 22:58:22 -0500

    Got to 4 plots and now doing sales in $

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -9,6 +9,11 @@ def main() :    #what do I want to solve here?
                 #figure out which warehouses produce late shipments
                 #figure out expected sales from year to year, including growth
                 #expected sales for each time of year
+
+    fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2, 2, gridspec_kw={'width_ratios': [1, 2]})
+    fig.suptitle("84 Lumber Co. Dashboard")
+    fig.tight_layout()
+    plt.setp(ax1.get_xticklabels(), rotation=-30, ha='left')
     
     #figure out late shipments by warehouse (step 1-4 done in preprocessing)
     #step 1: read in data
@@ -20,7 +25,7 @@ def main() :    #what do I want to solve here?
 
     #I want to do this only on the last month of data
     sales = pd.read_csv("US_Regional_Sales_Data_PreProcessed_To_Use.csv", sep=',')
-    sales.drop(['Unnamed: 0'], axis=1)
+    sales.drop(['Unnamed: 0'], axis=1, inplace=True)
     warehouses = sales.WarehouseCode.unique()
     warehousedata = []
     averages = ['AverageDaysToShip']
@@ -40,13 +45,13 @@ def main() :    #what do I want to solve here?
         maxs.append(days.max()[0])
         mins.append(days.min()[0])
 
-    warehousedata2 = [averages, medians, maxs, mins]
-
-    #print(warehousedata2)
-    #print([' '] + list(warehouses))
+    #warehousedata2 = [averages, medians, maxs, mins]
 
     #print(pd.DataFrame(warehousedata2, columns=[' '] + list(warehouses)))
-    #print(pd.DataFrame(warehousedata, columns=['WarehouseCode', 'AverageDaysToShip', 'MedianDaysToShip', 'MaxDaysToShip', 'MinDaysToShip']))
+    warehousedata = pd.DataFrame(warehousedata, columns=['WarehouseCode', 'AverageDaysToShip', 'MedianDaysToShip', 'MaxDaysToShip', 'MinDaysToShip'])
+    ax1.bar(warehousedata['WarehouseCode'], warehousedata['AverageDaysToShip'], label='Warehouse Code')
+    ax1.set_ylabel("Average Days to Ship Order")
+    ax1.set_title("Average of Days to Ship by Warehouse")
     
     
     #the numbers are very similar, maybe check to see from month to month how this changes. This also tells us its not a warehouse specific issue. It may be best reevaluate how
@@ -55,33 +60,42 @@ def main() :    #what do I want to solve here?
     #lets see month to month how it goes
     sales['DaysToShip'] = [int(x[:2]) for x in sales['DaysToShip']]
     sales['OrderDate'] = [datetime.datetime.strptime(d, "%m/%d/%Y") for d in sales['OrderDate']]
-    sales.insert(13, 'OrderMonth', [int(i.month) for i in sales["OrderDate"]], allow_duplicates=True)
-    print(sales['OrderMonth'])
+    sales.insert(12, 'OrderMonth', [int(i.month) for i in sales["OrderDate"]], allow_duplicates=True)
     sales.sort_values('OrderMonth', inplace = True)
-    print(sales)
 
     months = [x + 1 for x in range(12)]
     monthlywarehouse = sales[['WarehouseCode', 'DaysToShip', 'OrderMonth']].groupby(['OrderMonth', 'WarehouseCode']).mean().reset_index()
     print(monthlywarehouse)
+   
+
+
+    
+
+    size = plt.subplots(figsize = (15,15))
 
     X = months
-    Ygirls = [10,20,20,40] 
-    Zboys = [20,30,25,30] 
     
-    X_axis = np.arange(len(X)) 
+    X_axis = np.arange(1, 13, 1) 
     print(X_axis)
 
-    i = 0.125
-    for y in warehouses :
-        plt.bar(X_axis-(.5) + i, monthlywarehouse[(monthlywarehouse['WarehouseCode'] == y)]['DaysToShip'], 0.15, label = y)
-        i = i + 0.125
-    
-    plt.xticks(X_axis, X) 
-    plt.xlabel("Month") 
-    plt.ylabel("Average Days to Ship") 
-    plt.title("Monthly Average of Days to Ship by Warehouse") 
-    plt.legend() 
-    #plt.show() 
+    #add trendline to plot
+
+    i = 0.1
+    for ware in warehouses :
+        y = monthlywarehouse[(monthlywarehouse['WarehouseCode'] == ware)]['DaysToShip']
+        z = np.polyfit(X, y, 4)
+        p = np.poly1d(z)
+        ax2.scatter(X, y, label = ware)
+        ax2.plot(X, p(X))
+        #ax1.plot(X_axis-(.5) + i, monthlywarehouse[(monthlywarehouse['WarehouseCode'] == y)]['DaysToShip'], 0.1, label = y)
+        i = i + 0.1
+
+    ax2.set_xlabel("Month") 
+    ax2.set_ylabel("Average Days to Ship Order") 
+    ax2.set_title("Monthly Average of Days to Ship by Warehouse") 
+    ax2.legend()
+    ax2.set_xticks(np.arange(len(X_axis)+1))
+    fig.show()
 
     #expected sales based on time
     #step 2: Break down the data by year
@@ -91,20 +105,53 @@ def main() :    #what do I want to solve here?
         #create new df for each year
         #sum sales for that year
 
-    sales.insert(14, 'OrderYear', [int(i.year) for i in sales["OrderDate"]], allow_duplicates=True)
-    yearlysales = sales[['OrderDate', 'OrderYear']].groupby(['OrderYear']).count().reset_index()
+    sales.insert(13, 'OrderYear', [int(i.year) for i in sales["OrderDate"]], allow_duplicates=True)
+    yearlysales = sales[['Order Quantity', 'OrderYear']].groupby(['OrderYear']).sum().reset_index()
     print(yearlysales)
+
+    ax3.scatter(yearlysales['OrderYear'], yearlysales['Order Quantity'], label="ogga")
+    ax3.set_xticks(yearlysales['OrderYear'])
 
     #step 3: find sales by month
         #we can loop through each year, loop through each month
         # and find total sales
         # then we have monthly sales for each month in each year 
 
+    #print([(quan, cost, price, disc) for [quan, cost, price, disc] in zip(sales["Order Quantity"],sales["Unit Cost"], sales["Unit Price"], sales["Discount Applied"]) if float(float(quan) * ((float(price.replace(",", "")) * (1-float(disc))) - float(cost.replace(",", "")))) < 0])
+
+    sales.insert(14, 'Profits', [float(float(quan) * ((float(price.replace(",", "")) * (1-float(disc))) - float(cost.replace(",", "")))) for [quan, cost, price, disc] in zip(sales["Order Quantity"],sales["Unit Cost"], sales["Unit Price"], sales["Discount Applied"])])
     
-    monthlyyearlysales = sales[['OrderMonth', 'OrderYear', 'OrderDate']].groupby(['OrderMonth', 'OrderYear']).count().reset_index()
-    monthlyyearlysales.drop(monthlyyearlysales[monthlyyearlysales.OrderDate < 100].index, inplace = True)
+    monthlyyearlysales = sales[['OrderMonth', 'OrderYear', 'Order Quantity']].groupby(['OrderMonth', 'OrderYear']).sum().reset_index()
+    monthlyyearlysales.drop(monthlyyearlysales[monthlyyearlysales['Order Quantity'] < 100].index, inplace = True)
     print(monthlyyearlysales)
-    
+
+    i = 0.3
+    for year in np.sort(monthlyyearlysales['OrderYear'].unique()) :
+        y = monthlyyearlysales[(monthlyyearlysales['OrderYear'] == year)]['Order Quantity']
+        z = np.polyfit(X[12-len(y):], y, 1)
+        p = np.poly1d(z)
+        i = 12-len(y)
+        while len(y) < 12 :
+            #print(p(i))
+            y = pd.DataFrame([int(p(i))] + list(y))
+
+            i = i + 1
+        
+        print(len(y))
+        print(y)
+        #ax4.bar(X, y, label = year)
+        #ax4.plot(X, p(X))
+        ax4.bar(X_axis-(.63/2) + i, y, 0.33/2, label = year)
+        i = i + (0.33/2)
+
+    ax4.set_xlabel("Month") 
+    ax4.set_ylabel("Total Number of Sales") 
+    ax4.set_title("Monthly Sales Over 3 Years") 
+    #ax4.legend()
+    ax4.set_xticks(np.arange(len(X_axis)+1))
+    fig.show()
+
+    plt.show()    
     #step 4: figure out which months have the highest sales,
     # but check against multiple years to make sure its a trend
         #this will come from graphing it, figure that out later
